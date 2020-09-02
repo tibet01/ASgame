ee1e97f159fee14b9d645af87a282af4df638de3
tibet01 <tibet_rintharak@hotmail.co.th>
2020-09-02 11:49:30 +0700

    1

--- ASgame/LAB4.cpp
+++ ASgame/LAB4.cpp
@@ -1,12 +1,40 @@
 #include<stdio.h>
+#include<windows.h>
+#include<conio.h>
 
-void draw_ship()
+void erase_ship(int x, int y)
 {
-	printf(" <-o-> ");
+	COORD c = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf("       ");
+}
+
+void draw_ship(int x,int y)
+{
+	COORD c = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf("<-o->");
 }
 
 int main()
 {
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do
+	{
+		if (_kbhit())
+		{
+			ch = _getch();
+			if (ch == 'a' && x!=0) { erase_ship(x, y); draw_ship(--x, y); }
+			if (ch == 'd' && x != 75) { erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 'w') { erase_ship(x, y); draw_ship(x, --y); }
+			if (ch == 's') { erase_ship(x, y); draw_ship(x, ++y); }
 
+			fflush(stdin);
+		}
+		Sleep(100);
+	} while (ch != 'x');
+	printf("%d", x);
 	return 0;
 }
