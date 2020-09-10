d39dec881ee548c3e744fe02b684a5cc02a0ceaa
tibet01 <tibet_rintharak@hotmail.co.th>
2020-09-10 11:59:43 +0700

    1

--- ASgame/LAB4.cpp
+++ ASgame/LAB4.cpp
@@ -16,8 +16,32 @@ void draw_ship(int x,int y)
 	printf("<-o->");
 }
 
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO IpCursor;
+	IpCursor.bVisible = visible;
+	IpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console,&IpCursor);
+}
+
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
+
 int main()
 {
+	setcursor(0);
+	for (int i = 1; i <= 1600; i++)
+	{
+		setcolor(2,4 );
+		if (i%80==0)
+		{
+			printf("\n");
+		}
+	}
 	char ch = ' ';
 	int x = 38, y = 20;
 	draw_ship(x, y);
